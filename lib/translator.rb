f843f519d9f85514daa3d6287ed08a9c1862508d
Steven McAllister <steved1mac@gmail.com>
2020-07-10 17:06:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,7 @@ def get_japanese_emoticon(path, emoticon)
        emoticons = load_library(path)
        emoticons.each do |key, value|
          binding.pry
-            if emoticons[[value] ==  emoticon
+            if value[:english] ==  emoticon
            return key
          end 
       
